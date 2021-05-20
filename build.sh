ee9c189ce6ff205855c2970c86182a7543324702
ash <austin@theaus.xyz>
2021-05-20 14:56:56 -0600

    gpg signing go brr

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+gpg --receive-keys ABAF11C65A2970B130ABE3C479BE3E4300411886 647F28654894E3BD457199BE38DBBDC86092693E A2FF3A36AAA56654109064AB19802F8B0D70FC30 8657ABB260F056B1E5190839D9C4D26D0E604491 CEACC9E15534EBABB82D3FA03353C9CEF108B584
 mkdir output
 
 cd linux-zen
-makepkg -s --noconfirm
+MAKEFLAGS="-j$(nproc)" makepkg -s --noconfirm
 
 cd ../edk2
-makepkg -s --noconfirm
+MAKEFLAGS="-j$(nproc)" makepkg -s --noconfirm
 
 cd ../qemu
-makepkg -s --noconfirm
+MAKEFLAGS="-j$(nproc)" makepkg -s --noconfirm
 
 cd ..
 mv */*.pkg.tar.zst ./output/
