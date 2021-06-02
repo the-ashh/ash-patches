89b87b2abfe230e08d5e5d6a949e80a8f004ea4a
aus <austin@theaus.xyz>
2021-06-02 04:27:26 -0600

    submodule'd my keyring package

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 gpg --receive-keys ABAF11C65A2970B130ABE3C479BE3E4300411886 647F28654894E3BD457199BE38DBBDC86092693E A2FF3A36AAA56654109064AB19802F8B0D70FC30 8657ABB260F056B1E5190839D9C4D26D0E604491 CEACC9E15534EBABB82D3FA03353C9CEF108B584
 mkdir -p output
 
+git submodule init
+
 cd linux-zen
 MAKEFLAGS="-j$(nproc)" makepkg -sf --noconfirm
 
@@ -11,6 +13,9 @@ MAKEFLAGS="-j$(nproc)" makepkg -sf --noconfirm
 
 cd ../qemu
 MAKEFLAGS="-j$(nproc)" makepkg -sf --noconfirm
+
+cd ../ash-keyring
+makepkg -sf --noconfirm
 
 cd ../
 cp */*.pkg.tar.zs* output
